74d86f8c9ffab27fdc401701adbe8641c28b04e3
jthidalgojr <jthidalgojr@gmail.com>
2015-10-23 22:49:38 -0700

    fix schema building for bools and datetimes

--- polls/dataimport.py
+++ polls/dataimport.py
@@ -44,13 +44,16 @@ def get_type(value):
     # get best type match from most to least specific
     try:
         a = dict(value)
-        return (0, build_schema_sample(value))
+        return (-1, build_schema_sample(value))
     except:
         pass
 
     try:
         a = int(value)
-        return (1,"int")
+        if a in [0, 1]:
+            return (0, "bool")
+        else:
+            return (1,"int")
     except:
         pass
 
@@ -61,7 +64,7 @@ def get_type(value):
         pass
 
     try:
-        a = datetime.datetime.strptime( value, "%Y-%m-%dT%H:%M:%SZ" )
+        a = datetime.datetime.strptime( value, "%Y-%m-%dT%H:%M:%S" )
         return (3, "datetime")
     except:
         pass
@@ -84,7 +87,7 @@ def invert_to_dictionary(list):
     return d
 
 def extract_best_type(list):
-    if list[0][0] == 0: # nested objects
+    if list[0][0] == -1: # nested objects
         return combine_schemas([dict_tuple[1] for dict_tuple in list])
     else:
         return max(list, key=lambda x: x[0])[1]
